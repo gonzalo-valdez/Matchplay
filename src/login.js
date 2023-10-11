ac5aed16ea369638755b23067b3b01b7d34c752e
Gonzalo V <gvaldez-99@hotmail.com>
2023-10-11 23:32:16 +0200

    Login & Register

---
+++ src/login.js
@@ -0,0 +1,9 @@
+function openRegister(){
+    document.getElementById("login").style.display = "none";
+    document.getElementById("register").style.display = "block";
+}
+
+function closeRegister(){
+    document.getElementById("login").style.display = "block";
+    document.getElementById("register").style.display = "none";
+}
