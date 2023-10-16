178826054562ed0b830810aa41e3929f0bc7ae39
Gonzalo V <gvaldez-99@hotmail.com>
2023-10-16 13:54:53 +0200

    forms frontend

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function openRegister(){
+    $("#login").hide()
+    $("#register").show()
+}
+
+function closeRegister(){
+    $("#login").show()
+    $("#register").hide()
+}
--- src/login.js
+++
@@ -1,9 +0,0 @@
-function openRegister(){
-    document.getElementById("login").style.display = "none";
-    document.getElementById("register").style.display = "block";
-}
-
-function closeRegister(){
-    document.getElementById("login").style.display = "block";
-    document.getElementById("register").style.display = "none";
-}
